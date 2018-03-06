e20403c90a67f958fc725e00fbafff4bb37f9654
vincent <wf334934@163.com>
2018-03-06 23:51:11 +0800

    修复贴皮肤的bug，目前已经可以真正贴皮肤

--- src/app/basic/image/image.component.ts
+++ src/app/basic/image/image.component.ts
@@ -12,6 +12,9 @@ export class ImageComponent implements OnInit {
   canvas: any; // 待动态创建的canvas
   myImg = new Image(); // 图像实例
   option = null; // echarts的数据options
+  zPoints: any = [];
+  imgData: any;
+  data: any = [];
   constructor() {
     // 图像的路径
     this.myImg.src = '../../../assets/data/webwxgetmsgimg.jpg';
@@ -25,35 +28,13 @@ export class ImageComponent implements OnInit {
 
     this.myImg.onload = () => {
       // 获取图像宽度
-      const width = this.canvas.width = this.myImg.height;
+      const width = this.canvas.width = this.myImg.width;
       // 获取图像高度
       const height = this.canvas.height = this.myImg.height;
       // 在canvas上画图像
       ctx.drawImage(this.myImg, 0, 0, width, height);
       // 获得canvas上图像的数据
-      const imgData = ctx.getImageData(0, 0, width, height);
-      // 图像数据存储的数据
-      const data = [];
-
-      let r: any;
-      let g: any;
-      let b: any;
-      let lum: any;
-      // 将图像的数据转换为RGB格式
-      for ( let i = 0; i < imgData.data.length / 4; i++) {
-        r = imgData.data[i * 4]; // 每个像素点的red
-        g = imgData.data[i * 4 + 1]; // 每个像素点的green
-        b = imgData.data[i * 4 + 2]; // 每个像素点的blue
-
-        // 这就是我们设置三个元素的向量，为我们的亮度来保存颜色比重的地方。这三个值加起来要为 1，
-        // 这样我们才能把亮度计算为 0.0 - 1.0 之间的值。注意中间的值，就是表示绿色的值，用了
-        // 70% 的颜色比重，而蓝色只用了它的 10%。这是SONY Trinitron的数据，
-        // 更加一般的系数是ITU HDTV标准 0.2125, 0.7154, 0.0721以及用于CRT显示器非线性色彩的0.299, 0.587, 0.114）。
-        lum = 255 - ( 0.2125 * r + 0.7154 * g + 0.0721 * b);
-        lum = (lum - 125) / 10 + 50;
-        // 转换为x,y,z存入
-        data.push([i % width, height - Math.floor(i / width), lum]);
-      }
+      this.imgData = ctx.getImageData(0, 0, width, height);
 
       this.option = {
         tooltip: {},
@@ -67,7 +48,7 @@ export class ImageComponent implements OnInit {
         zAxis3D: {
           type: 'value',
           min: 0,
-          max: 100
+          max: 3000
         },
         grid3D: {
           axisPointer: {
@@ -103,16 +84,47 @@ export class ImageComponent implements OnInit {
           itemStyle: {
               color: (params) => {
                   const i = params.dataIndex;
-                  const red = imgData.data[i * 4];
-                  const green = imgData.data[i * 4 + 1];
-                  const blue = imgData.data[i * 4 + 2];
+                  const red = this.imgData.data[i * 4];
+                  const green = this.imgData.data[i * 4 + 1];
+                  const blue = this.imgData.data[i * 4 + 2];
                   return 'rgb(' + [red, green, blue].join(',') + ')';
               }
           },
-          data: data
+          data: this.data
       }]
       };
       // this.myChart.setOption(this.option); // 刷新数据
     };
+  }
+
+  readZFile(fileInfo) {
+    const fileName = fileInfo.target.files[0];
+    if (!fileName) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = (file) => {
+      const context: any = file.target; // 获取文件目标
+      const text = context.result; // 将文件转换为文本字符串
+      const allLines = text.split(/\r\n|\n/); // 将字符串按行转换为数组
+      allLines.map( (line) => {
+        if (line) {
+          const z = parseFloat(line);
+          this.zPoints.push(z);
+        }
+      });
+      // 这里更新数据
+      // this.readImage();
+      const w = this.myImg.width;
+      const h = this.myImg.height;
+      for ( let i = 0; i < this.imgData.data.length / 4; i++) {
+
+        // 转换为x,y,z存入
+        this.data.push([i % w, h - Math.floor(i / w), this.zPoints[i]]);
+      }
+      this.option.series.data = this.data;
+      this.myChart.setOption(this.option, false);
+    };
+    reader.readAsText(fileName);
   }
 }
