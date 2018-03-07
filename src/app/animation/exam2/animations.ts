7ea0709860bacc8c2d003194845a86d061d1d3b1
vincent <wf334934@163.com>
2018-03-07 23:02:54 +0800

    完成关键帧的练习

--- src/app/animation/exam2/animations.ts
+++ src/app/animation/exam2/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 // 定义一个动画，左右两个状态加上颜色变化更容易理解吧
 export const boxAnimate2 = trigger('box', [
@@ -6,4 +6,28 @@ export const boxAnimate2 = trigger('box', [
     state('left', style({ transform: 'translate3d(0,0,0)', background: 'red' })),
     // 定义另外一个状态right
     state('right', style({ transform: 'translate3d(200%,0,0)', background: 'blue' })),
-  ]);
+    transition('* => *', animate(500)),
+]);
+
+export const KeyframesAnimate = trigger('KeyframesAnimate', [
+    // 入场动画
+    transition(':enter', [
+      animate(500, keyframes([
+        // 根据offset，translate3d的不同实现跳跃，translate3d为移动
+        style({opacity: 0, transform: 'translate3d(-400%,0,0)', offset: 0}),
+        style({opacity: 0.5, transform: 'translate3d(-150%,-50%,0)',  offset: 0.3}),
+        style({opacity: 1, transform: 'translate3d(0,10%,0)',  offset: 0.7}),
+        style({opacity: 1, transform: 'translate3d(0,0,0)',     offset: 1.0})
+      ]))
+    ]),
+    // 出场动画
+    transition(':leave', [
+      animate(500, keyframes([
+        style({opacity: 1, transform: 'translate3d(0,0,0)',     offset: 0}),
+        style({opacity: 1, transform: 'translate3d(0,10%,0)', offset: 0.3}),
+        style({opacity: 0.5, transform: 'translate3d(150%,-50%,0)',  offset: 0.7}),
+        style({opacity: 0, transform: 'translate3d(400%,0,0)',  offset: 1.0})
+      ]))
+    ]),
+
+]);
--- src/app/animation/exam2/exam2.component.ts
+++ src/app/animation/exam2/exam2.component.ts
@@ -1,24 +1,35 @@
 import { animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { boxAnimate2 } from './animations';
+import { boxAnimate2, KeyframesAnimate } from './animations';
 
 @Component({
   selector: 'app-exam2',
   templateUrl: './exam2.component.html',
   styleUrls: ['./exam2.component.css'],
   animations: [
-    boxAnimate2
+    boxAnimate2,
+    KeyframesAnimate
   ]
 })
 export class Exam2Component implements OnInit {
 
   public boxState = '';
+
+  // 显示么
+  show = true;
+  num = 123;
+  Keyframes = true;
+  Group = true;
   constructor() { }
 
   ngOnInit() {
   }
 
-  changeState(state: string) {
-    this.boxState = state;
+  KeyframesShow() {
+    this.Keyframes = !this.Keyframes;
+  }
+
+  changShow() {
+    this.show = !this.show;
   }
 }
